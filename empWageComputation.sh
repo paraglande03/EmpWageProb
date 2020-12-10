2aa2f0bb1a193fa5358e605b178a1312142168a9
paraglande03 <paraglande03@gmail.com>
2020-12-10 17:56:24 +0530

    Add part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 ratePerHr=20
 fullDayHr=8
-random=$(($RANDOM%2))
+partTimeHr=4
+dayType=$((RANDOM%2))
+random=$((RANDOM%2))
 	if (( $random==0 ))
 	then 
 	echo "Employee is Present"
-	dailywage=$(($ratePerHr*$fullDayHr))
-
-	echo "Daily wage of employee is" $dailywage
-	else
+		if (($dayType==0))
+		then
+		dailyPartwage=$(($partTimeHr*$ratePerHr))
+		echo "emplyee is part time and dailywage is $dailyPartwage"
+		else
+		dailyFullwage=$(($ratePerHr*$fullDayHr))
+		echo "employee is full time and dailywage is $dailyFullwage"
+		fi
+		else
 	     echo "Employee is Absent"
 	fi
 	
